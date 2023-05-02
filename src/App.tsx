4df8b8ecb13d1024fd79b8befaac4b343ba061cb
ssori <115159126+ssori0421@users.noreply.github.com>
2023-05-02 23:14:48 +0900

    feat: 페이지 기본 퍼블리싱 및 get 요청 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { getDisease } from './service/getDisease'
 
 function App() {
-  return <div></div>
+  const [searchInput, setSearchInput] = useState('')
+
+  const onChangeSearchInput = async (e: any) => {
+    setSearchInput(e.target.value)
+    const { data } = await getDisease(e.target.value)
+    return data
+  }
+
+  return (
+    <>
+      <div>
+        <input type="text" onChange={onChangeSearchInput} />
+        <button>검색</button>
+      </div>
+      <div>
+        <p>간세포암</p>
+        <p>간세포암</p>
+        <p>간세포암</p>
+        <p>간세포암</p>
+      </div>
+    </>
+  )
 }
 
 export default App

1148c842d7af35bcbd32c1d68a48466bc12eaaaa
sabit1997 <yejiii1997@gmail.com>
2023-05-03 16:51:35 +0900

    feat: 스타일 추가 및 레이아웃 변경
    - SearchInput, SearchResult import해 적용
    - PageWrapper , SearchSectionWrapper 스타일 추가 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import './App.css'
+import styled from 'styled-components'
+
+import SearchInput from './components/SearchInput'
+import SearchResult from './components/SearchResult'
 import GlobalStyle from './styles/GlobalStyle'
+import { backgroundBlue } from './styles/mixins'
+
+const PageWrapper = styled.div`
+  ${backgroundBlue}
+  width: 100%;
+  height: 100vh;
+`
+
+const SearchSectionWrapper = styled.div`
+  width: 480px;
+  margin: 0 auto;
+  padding-top: 100px;
+`
 
 function App() {
   return (
-    <div>
+    <PageWrapper>
       <GlobalStyle />
-    </div>
+      <SearchSectionWrapper>
+        <SearchInput />
+        <SearchResult />
+      </SearchSectionWrapper>
+    </PageWrapper>
   )
 }
 

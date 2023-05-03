ebf6111cbe4650fe34def945acd752d2751900ae
LJJ <wjdrk70@gmail.com>
2023-05-03 19:12:11 +0900

    refactor: SearchBar App 이 수정안되어서 다시 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import SearchBar from './components/search/SearchBar'
 function App() {
   return (
     <div className="App">
-      <SearchBar
-        onSearch={(query: string): void => {
-          throw new Error('Function not implemented.')
-        }}
-      />
+      <SearchBar />
     </div>
   )
 }
--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -3,11 +3,7 @@ import React, { FC } from 'react'
 import SearchSuggestion from './SearchSuggestion'
 import { Banner, Wrap } from './style'
 
-type SearchBarProps = {
-  onSearch: (query: string) => void
-}
-
-const SearchBar: FC<SearchBarProps> = () => {
+const SearchBar: FC = () => {
   return (
     <Wrap>
       <Banner>

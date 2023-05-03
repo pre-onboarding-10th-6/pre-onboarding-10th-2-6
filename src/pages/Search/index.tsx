1aa592e56bbb1c6658d193bc0ad7fcde5973b440
jooyoung <jooyoung@pood.pet>
2023-05-03 13:06:40 +0900

    feat: cache storage에 저장

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -8,35 +8,41 @@ interface SEARCH_ITEM {
 
 const Search = () => {
   const [searchKeyword, setSearchKeyword] = useState<string>('')
-  const [result, setResult] = useState<[]>([])
-  const [loading, setLoading] = useState(true)
+  const [result, setResult] = useState<SEARCH_ITEM[]>([])
+  //   const [loading, setLoading] = useState(true)
   const handleChange = (e: React.ChangeEvent<HTMLElement>) => {
     const value = (e.target as HTMLInputElement).value
     setSearchKeyword(value)
   }
 
   const BASE_URL = `/api/v1/search-conditions/?name=${searchKeyword}`
+
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true)
+    async function handleSearch() {
+      const cacheStorage = await caches.open('search')
+      const cachedData = await cacheStorage.match(BASE_URL)
       try {
-        const res = await axios.get(BASE_URL)
-        console.log(res.data)
-        setResult(res.data)
-      } catch (error) {
-        console.error(error)
+        if (cachedData) {
+          const res = await cachedData.json()
+          setResult(res)
+        } else {
+          const res = await axios.get(BASE_URL)
+          cacheStorage.put(BASE_URL, new Response(JSON.stringify(res.data)))
+          setResult(res.data)
+        }
+      } catch (err) {
+        console.error(err)
       }
     }
-    fetchData()
+    handleSearch()
   }, [searchKeyword])
 
   return (
     <main>
       <input value={searchKeyword} onChange={handleChange} />
-      <button />
-      {result.map((item: SEARCH_ITEM) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      <button>검색</button>
+      {Array.isArray(result) &&
+        result.map((item: SEARCH_ITEM) => <div key={item.id}>{item.name}</div>)}
     </main>
   )
 }

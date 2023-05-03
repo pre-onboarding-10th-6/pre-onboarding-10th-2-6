a7715acf6760500dad37afc6bbab6ab00b8218dc
sabit1997 <yejiii1997@gmail.com>
2023-05-03 22:55:42 +0900

    feat: 추천 검색어 기능 추가 및 자식 컴포넌트로 props 전달

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { debounce } from 'lodash'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
+import searchAPI from './api/searchAPI'
 import SearchInput from './components/SearchInput'
 import SearchResult from './components/SearchResult'
 import GlobalStyle from './styles/GlobalStyle'
@@ -16,14 +19,39 @@ const SearchSectionWrapper = styled.div`
   margin: 0 auto;
   padding-top: 100px;
 `
+export interface Data {
+  name: string
+  id: number
+}
 
 function App() {
+  const [keyword, setKeyword] = useState<string>('')
+  const [results, setResults] = useState<Data[]>([])
+
+  const search = async (word: string) => {
+    try {
+      const response = await searchAPI(word)
+      const data = await response.data
+      setResults(data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const debouncedSearch = debounce(search, 200)
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const keyword = e.target.value
+    setKeyword(keyword)
+    debouncedSearch(keyword)
+  }
+
   return (
     <PageWrapper>
       <GlobalStyle />
       <SearchSectionWrapper>
-        <SearchInput />
-        <SearchResult />
+        <SearchInput value={keyword} onChange={handleInputChange} />
+        {keyword === '' ? null : <SearchResult results={results} />}
       </SearchSectionWrapper>
     </PageWrapper>
   )

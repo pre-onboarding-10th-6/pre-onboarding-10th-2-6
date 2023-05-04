54aa784d41afaa2928d99c4dfc790d185b563fb1
WonhyeongLee <54429762+WonhyeongLee@users.noreply.github.com>
2023-05-04 22:20:00 +0900

    fix: proxy부분 / 빼먹은 오타 수정

--- src/api/search.ts
+++ src/api/search.ts
@@ -3,4 +3,4 @@ import { Disease } from '../types/disease'
 import { instance } from '.'
 const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 export const getDiseases = (name: string): Promise<Disease[]> =>
-  instance.get(`${PROXY}api/v1/search-conditions/`, { params: { name } })
+  instance.get(`${PROXY}/api/v1/search-conditions/`, { params: { name } })

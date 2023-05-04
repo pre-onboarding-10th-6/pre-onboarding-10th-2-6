0ca58b5b865676a6f4737cf9d77937d10e07a3fb
WonhyeongLee <bigfile57@naver.com>
2023-05-04 22:16:48 +0900

    fix: netlify.toml로 프록시 설정, api요청에 PROXY추가

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,6 +1,6 @@
 import { Disease } from '../types/disease'
 
 import { instance } from '.'
-
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 export const getDiseases = (name: string): Promise<Disease[]> =>
-  instance.get('api/v1/search-conditions/', { params: { name } })
+  instance.get(`${PROXY}api/v1/search-conditions/`, { params: { name } })

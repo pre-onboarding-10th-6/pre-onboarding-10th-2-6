346a8f50efba046182b475136f4b252270d48f06
ssori <115159126+ssori0421@users.noreply.github.com>
2023-05-04 12:29:11 +0900

    feat: 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function App() {
     try {
       const data = await getDisease(debounceInput)
       setSuggestList(data)
-      console.info('calling api')
     } catch (error) {
       console.error(error)
       setSuggestList(undefined)
@@ -76,36 +75,4 @@ const StTitle = styled.h2`
   font-size: 1.5rem;
   line-height: 120%;
   margin-bottom: 30px;
-`
-
-const StInputContainer = styled.div`
-  display: flex;
-  align-items: center;
-  position: relative;
-
-  background-color: ${color.WHITE};
-  border: 1px solid ${color.LIGHT_GRAY};
-  border-radius: 30px;
-  padding: 10px;
-  margin-bottom: 8px;
-
-  button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 8px;
-    color: ${color.WHITE};
-    background-color: ${color.BUTTON};
-    border-radius: 100%;
-  }
-`
-const StInputWrap = styled.div`
-  display: flex;
-  gap: 8px;
-  align-items: center;
-  width: 100%;
-  height: 30px;
-  input {
-    width: 100%;
-  }
 `

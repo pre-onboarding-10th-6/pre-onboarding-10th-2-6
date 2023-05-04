8562c3e76f2897a85b17e36c856cc61bfa227555
LJJ <wjdrk70@gmail.com>
2023-05-04 20:51:54 +0900

    proxy 수정6

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -69,9 +69,9 @@ const Search = () => {
   return (
     <SearchLayout>
       <h1>
-        !!!!국내 모든 임상시험 검색하고
+        국내 모든 임상시험 검색하고
         <br />
-        온라인으로 참여하기!!!!
+        온라인으로 참여하기
       </h1>
       <section className="search-section">
         <SearchBar
